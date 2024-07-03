03bb92e478715a79216104e7bc472313738f8c0f
dantetarraga <dante.tarragau@gmail.com>
2024-07-03 10:27:27 -0500

    feat: Agregar soporte para dispositivos móviles en el componente Sidebar

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
+import IconMenu from '../icons/IconMenu'
 
 const NAVIGATION_ITEMS = [
   { label: 'Home', path: '/' },
@@ -7,28 +8,46 @@ const NAVIGATION_ITEMS = [
   { label: 'Vista 2', path: '/vista2' }
 ]
 
-const Sidebar = () => {
+const Sidebar = ({ isShow, isMobile, onClose }) => {
   return (
-    <nav
-      className='h-full md:flex flex-col bg-white border-r shadow-sm gap-4 p-4 hidden'
-    >
-      <div className='text-center border-b py-2'>
-        <img src={Logo} alt='Logo' className='w-32 mx-auto' />
-        <h1 className='font-bold uppercase text-lg text-indigo-400'>World Countries</h1>
-      </div>
-      {NAVIGATION_ITEMS.map((item) => (
-        <NavLink
-          key={item.label}
-          to={item.path}
-          className={({ isActive }) => `w-60 text-center py-2 px-3 font-semibold text-sm rounded-md cursor-pointer transition-colors ease-in-out duration-200
-          ${isActive
-            ? 'bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800'
-            : 'hover:bg-indigo-50 text-gray-600'}`}
-        >
-          {item.label}
-        </NavLink>
-      ))}
-    </nav>
+    <>
+      <div
+        className={`${!isShow && 'hidden'} bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 right-0 backdrop-blur-sm z-10 md:hidden`}
+        onClick={onClose}
+      />
+      <nav
+        className={`
+        fixed top-0 left-0 h-full w-72 bg-white border-r shadow-sm p-4
+        md:static md:block z-10 transition-transform duration-300 ease-in-out
+        ${isShow ? 'translate-x-0' : '-translate-x-full md:translate-x-0'}
+        `}
+      >
+        <div className='text-center border-b py-2'>
+          <img src={Logo} alt='Logo' className='w-32 mx-auto' />
+          <h1 className='font-bold uppercase text-lg text-indigo-400'>World Countries</h1>
+        </div>
+        {isMobile && (
+          <button className='block md:hidden absolute top-2 right-2 p-2 hover:bg-slate-200 rounded-full' onClick={onClose}>
+            <IconMenu />
+          </button>
+        )}
+        {NAVIGATION_ITEMS.map((item) => (
+          <NavLink
+            key={item.label}
+            to={item.path}
+            className={({ isActive }) => `
+        block w-full text-center py-2 px-3 my-2 font-semibold text-sm rounded-md cursor-pointer
+        transition-colors ease-in-out duration-200
+        ${isActive
+        ? 'bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800'
+        : 'hover:bg-indigo-50 text-gray-600'}
+        `}
+          >
+            {item.label}
+          </NavLink>
+        ))}
+      </nav>
+    </>
   )
 }
 
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,15 +1,24 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import IconMenu from '../components/icons/IconMenu'
 import CountryLoading from '../components/loading/CountryLoading'
 import Sidebar from '../components/ui/Sidebar'
+import { useMobile } from '../hooks/useMobile'
 
 const AppLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const { isMobile } = useMobile()
+
+  const handleShowSidebar = () => setIsSidebarOpen(!isSidebarOpen)
+
   return (
     <section className='flex h-screen'>
-      <Sidebar />
+      <Sidebar isShow={isSidebarOpen} onClose={handleShowSidebar} isMobile={isMobile} />
       <main className='w-full px-5 relative'>
-        <button className='block md:hidden absolute top-8 left-8 p-2 hover:bg-slate-200 rounded-full'>
+        <button
+          className='block md:hidden absolute top-8 left-8 p-2 hover:bg-slate-200 rounded-full'
+          onClick={handleShowSidebar}
+        >
           <IconMenu />
         </button>
         <Suspense fallback={<CountryLoading />}>
